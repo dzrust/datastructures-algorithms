01d3960c8e894f2e67003a3ec040432c4a7d1187
Daniel Zrust <danielzrust@gmail.com>
2025-08-29 13:34:35 -0500

    feat: updated to use a fast and slow pointer which iterates over everything only 1 time technically

--- linked-list/isPalindrome.js
+++ linked-list/isPalindrome.js
@@ -11,27 +11,41 @@ const { SinglyLinkedList, UniDirectionListNode } = require("../lib/SinglyLinkedL
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    let cur = head.next;
-    const reversedListTail = new UniDirectionListNode(head.val), sentinel = new UniDirectionListNode(-1);
-    sentinel.next = reversedListTail;
-    while (cur) {
-        const newNode = new UniDirectionListNode(cur.val);
-        newNode.next = sentinel.next;
-        sentinel.next = newNode;
-        cur = cur.next;
+var isPalindrome = function (head) {
+  let slow = head,
+    fast = head;
+  const sentinel = new UniDirectionListNode(-1);
+  let count = 1;
+  while (fast && fast.next) {
+    const newNode = new UniDirectionListNode(slow.val);
+    newNode.next = sentinel.next;
+    sentinel.next = newNode;
+    slow = slow.next;
+    fast = fast.next.next;
+    count += 2;
+  }
+  if (fast === null) {
+    count ++;
+  }
+  console.log(count);
+  let reverseCur = sentinel.next;
+  console.log(logList(reverseCur));
+  console.log(logList(fast === null ? slow : slow.next));
+  if (fast !== null) {
+    slow = slow.next;
+  }
+  while (slow && reverseCur) {
+    if (slow.val !== reverseCur.val) {
+      return false;
     }
-    cur = head;
-    let reverseCur = sentinel.next;
-    while (cur && reverseCur) {
-        if (cur.val !== reverseCur.val) {
-            return false;
-        }
-        cur = cur.next;
-        reverseCur = reverseCur.newNode
-    }
-    return true;
+    slow = slow.next;
+    reverseCur = reverseCur.next;
+  }
+  return true;
 };
 
 console.log(isPalindrome(new SinglyLinkedList([1, 2]).getHead()));
-console.log(isPalindrome(new SinglyLinkedList([1,2,2,1]).getHead()));
+console.log(isPalindrome(new SinglyLinkedList([1, 2, 2, 1]).getHead()));
+console.log(isPalindrome(new SinglyLinkedList([1, 0, 0]).getHead()));
+console.log(isPalindrome(new SinglyLinkedList([0, 1, 0]).getHead()));
+console.log(isPalindrome(new SinglyLinkedList([1, 2, 2, 2, 1]).getHead()));
