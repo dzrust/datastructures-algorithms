7db5a384144d9e02c6959e3f641ce1b6a61b2d7e
Daniel Zrust <danielzrust@gmail.com>
2025-09-01 12:16:44 -0500

    feat: added in a working solution that does not implement the head and tail functions

--- linked-list/myLinkedList.js
+++ linked-list/myLinkedList.js
@@ -1,12 +1,11 @@
 var ListNode = function (val) {
-  this.val = val;
-  this.next = null;
+    this.val = val;
+    this.next = null;
 };
 
 var MyLinkedList = function () {
-  this.head = new ListNode(-1);
-  this.tail = null;
-  this.length = 0;
+    this.head = null;
+    this.length = 0;
 };
 
 /**
@@ -14,16 +13,16 @@ var MyLinkedList = function () {
  * @return {number}
  */
 MyLinkedList.prototype.get = function (index) {
-  if (this.length < index) {
-    return -1;
-  }
-  let cur = this.head.next,
-    count = 0;
-  while (cur && count < index) {
-    cur = cur.next;
-    count++;
-  }
-  return cur ? cur.val : -1;
+    if (this.length <= index) {
+        return -1;
+    }
+    let cur = this.head,
+        count = 0;
+    while (cur && count < index) {
+        cur = cur.next;
+        count++;
+    }
+    return cur ? cur.val : -1;
 };
 
 /**
@@ -31,13 +30,10 @@ MyLinkedList.prototype.get = function (index) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtHead = function (val) {
-  const temp = new ListNode(val);
-  temp.next = this.head.next;
-  this.head.next = temp;
-  if (this.tail === null) {
-    this.tail = temp;
-  }
-  this.length++;
+    const newNode = new ListNode(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
 };
 
 /**
@@ -45,15 +41,19 @@ MyLinkedList.prototype.addAtHead = function (val) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtTail = function (val) {
-  const temp = new ListNode(val);
-  if (!this.tail) {
-    this.head.next = temp;
-    this.tail = temp;
-  } else {
-    this.tail.next = temp;
-    this.tail = temp;
-  }
-  this.length++;
+    let cur = this.head;
+    while (cur && cur.next) {
+        cur = cur.next;
+    }
+    const newNode = new ListNode(val);
+    if (cur?.next) {
+        cur.next.next = newNode;
+    } else if (cur) {
+        cur.next = newNode;
+    } else {
+        this.head = newNode;
+    }
+    this.length++;
 };
 
 /**
@@ -62,22 +62,27 @@ MyLinkedList.prototype.addAtTail = function (val) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtIndex = function (index, val) {
-  if (this.length < index) return;
-  let prev = this.head,
-    cur = this.head.next,
-    count = 0;
-  while (cur && count < index) {
-    prev = cur;
-    cur = cur.next;
-    count++;
-  }
-  const newNode = new ListNode(val);
-  prev.next = newNode;
-  prev.next.next = cur;
-  if (this.length === index) {
-    this.tail = newNode;
-  }
-  this.length++;
+    if (this.length < index) return;
+    if (index === 0) {
+        this.addAtHead(val);
+        return;
+    }
+    let prev = this.head,
+        cur = this.head,
+        count = 0;
+    while (cur && count < index) {
+        prev = cur;
+        cur = cur.next;
+        count++;
+    }
+    const newNode = new ListNode(val);
+    newNode.next = cur;
+    if (!prev) {
+        this.head = newNode;
+    } else {
+        prev.next = newNode;
+    }
+    this.length++;
 };
 
 /**
@@ -85,50 +90,65 @@ MyLinkedList.prototype.addAtIndex = function (index, val) {
  * @return {void}
  */
 MyLinkedList.prototype.deleteAtIndex = function (index) {
-  if (this.length < index || this.length === 0) return;
-  let prev = this.head,
-    cur = this.head.next,
-    count = 0;
-  while (cur && count < index) {
-    prev = cur;
-    cur = cur.next;
-    count++;
-  }
-  prev.next = prev.next.next;
-  if (this.length === index + 1) {
-    this.tail = prev.next;
-  }
-  this.length--;
-  if (this.length === 0) {
-    this.tail = null;
-  }
+    if (this.length <= index || this.length === 0) return;
+    if (index === 0) {
+        this.head = this.head.next;
+        return;
+    }
+    let prev = this.head,
+        cur = this.head,
+        count = 0;
+    while (cur && count < index) {
+        prev = cur;
+        cur = cur.next;
+        count++;
+    }
+    if (!prev?.next) {
+        this.head = null;
+    } else {
+        prev.next = prev.next.next;
+    }
+    this.length--;
 };
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * var obj = new MyLinkedList()
- * var param_1 = obj.get(index)
- * obj.addAtHead(val)
- * obj.addAtTail(val)
- * obj.addAtIndex(index,val)
- * obj.deleteAtIndex(index)
- */
+const runTestCase = (funcsToRun, testCaseValues) => {
+  const testCase = new MyLinkedList();
+  funcsToRun.forEach((func, index) => {
+    console.log(testCase[func](...testCaseValues[index]));
+  });
+};
+// runTestCase(
+//   [
+//     "addAtHead",
+//     "addAtHead",
+//     "addAtHead",
+//     "addAtIndex",
+//     "deleteAtIndex",
+//     "addAtHead",
+//     "addAtTail",
+//     "get",
+//     "addAtHead",
+//     "addAtIndex",
+//     "addAtHead",
+//   ],
+//   [[7], [2], [1], [3, 0], [2], [6], [4], [4], [4], [5, 0], [6]],
+// );
 
-const testCase = new MyLinkedList();
-const funcs = [
-  "addAtHead",
-  "addAtHead",
-  "addAtHead",
-  "addAtIndex",
-  "deleteAtIndex",
-  "addAtHead",
-  "addAtTail",
-  "get",
-  "addAtHead",
-  "addAtIndex",
-  "addAtHead",
-];
-const values = [[7], [2], [1], [3, 0], [2], [6], [4], [4], [4], [5, 0], [6]];
-funcs.forEach((func, index) => {
-  console.log(testCase[func](...values[index]));
-});
+// runTestCase(["addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"], [[1], [3], [1, 2], [1], [0], [0]]);
+// runTestCase(["addAtIndex", "addAtIndex", "addAtIndex", "get"], [[0, 10], [0, 20], [1, 30], [0]]);
+runTestCase(
+  [
+    "addAtHead",
+    "deleteAtIndex",
+    "addAtHead",
+    "addAtHead",
+    "addAtHead",
+    "addAtHead",
+    "addAtHead",
+    "addAtTail",
+    "get",
+    "deleteAtIndex",
+    "deleteAtIndex",
+  ],
+  [[2], [1], [2], [7], [3], [2], [5], [5], [5], [6], [4]],
+);
