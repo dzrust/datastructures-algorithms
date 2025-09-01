6658844e68496c3f6aeea03174fd21b56b208780
Daniel Zrust <danielzrust@gmail.com>
2025-09-01 15:21:30 -0500

    feat: add in the remove duplicates

---
+++ stacks-queues/removeDuplicates.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    const stack = [], {length} = s;
+    for (let i = 0; i < length; i++) {
+        stack.push(s[i]);
+        while (stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]) {
+            stack.pop(); // pop both
+            stack.pop();
+        }
+    }
+    return stack.join("");
+};
+
+/**
+ * 
+ * Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+
+ * 
+ */
+
+console.log(removeDuplicates("abbaca"));
+console.log(removeDuplicates("azxxzy"));
+console.log(removeDuplicates("aa"));
+console.log(removeDuplicates("aba"));
+console.log(removeDuplicates("a"));
+console.log(removeDuplicates("abbabbabbabba"));
+console.log(removeDuplicates("abbbaabbbaabbbaabbba"));
