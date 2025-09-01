48604dcd22242a1bd85403f935b200f096b04315
Daniel Zrust <danielzrust@gmail.com>
2025-09-01 15:45:27 -0500

    feat: add in the initial scaffolding for the makeGood problem

---
+++ stacks-queues/makeGood.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    
+};
+
+
+/**
+ * 
+ * Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+
+Example 2:
+
+Input: s = "abBAcC"
+Output: ""
+Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+"abBAcC" --> "aAcC" --> "cC" --> ""
+"abBAcC" --> "abBA" --> "aA" --> ""
+
+Example 3:
+
+Input: s = "s"
+Output: "s"
+
+ * 
+ */
